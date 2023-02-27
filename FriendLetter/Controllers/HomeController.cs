15bc8399bf20c5e7e112bac6c9a299f8d6cdff84
johnedisc <christopher.johnedis@gmail.com>
2023-02-27 00:06:19 -0800

    add route decoroation to home controller

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -4,6 +4,11 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
-    public string Hello() { return "que bola"; }
+    [Route("/hello")]
+    public string presentarle() { return "que bola"; }
+    [Route("/goodbye")]
+    public string decirleAdios() { return "que le cuide"; }
+    [Route("/")]
+    public string Letter() { return "la carta ahorita le pongo aca"; }
   }
 }
